0916408b94c98bb21316ea90f125f6ec9fcd864a
ismaiel <ismaiel.nab@gmail.com>
2020-09-18 18:03:07 +0200

    do the final commented changes as per the mentor

--- Week1/homework/js-exercises/ex3.js
+++ Week1/homework/js-exercises/ex3.js
@@ -3,12 +3,11 @@ document.getElementById('buttonH').addEventListener('click', useHttbReq);
 document.getElementById('buttonA').addEventListener('click', useAxios);
 
 // declaration of the required variables
-let outPut = document.getElementById('displayData');
-let image = document.getElementById('images');
+const outPut = document.getElementById('displayData');
+const image = document.getElementById('images');
 
 // create ul with DOM
 let pic = '<ul class="gallery"></ul>';
-let i = 0;
 
 // the function with XMLHttpRequest()//
 function useHttbReq() {
@@ -22,6 +21,8 @@ function useHttbReq() {
       pic += '<li ><img src="' + info.message + '"  width = "100" /></li>';
       image.innerHTML = pic;
 
+      // as per the comment
+      console.log(info.message);
       // error message
     } else if (this.status == 404) {
       outPut.innerText = 'Not Found .....';
@@ -37,6 +38,9 @@ function useAxios() {
     .then(res => {
       pic += '<li ><img src="' + res.data.message + '"  width = "100" /></li>';
       image.innerHTML = pic;
+
+      //As per the comment
+      console.log(res.data.message);
     })
     .catch(err => console.log(err));
 }
--- Week1/homework/js-exercises/xkcd.js
+++ Week1/homework/js-exercises/xkcd.js
@@ -3,8 +3,8 @@ document.getElementById('buttonH').addEventListener('click', useHttbReq);
 document.getElementById('buttonA').addEventListener('click', useAxios);
 
 // declaration of the required variables
-let outPut = document.getElementById('displayData');
-let image = document.getElementById('image');
+const outPut = document.getElementById('displayData');
+const image = document.getElementById('image');
 
 // the function with XMLHttpRequest()//
 function useHttbReq() {
@@ -13,8 +13,9 @@ function useHttbReq() {
   xhr.onload = () => {
     if (xhr.status == 200) {
       let info = JSON.parse(xhr.responseText);
-      let pic = '';
-      pic += '<img src="' + info.img + '"  />';
+
+      // changed as per the comments
+      let pic = '<img src="' + info.img + '"  />';
       image.innerHTML = pic;
       console.log(info);
     } else if (this.status == 404) {
